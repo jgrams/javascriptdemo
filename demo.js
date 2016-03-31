db284d56ecff1d6d6a7bcc327258c78e7013730b
John Gramila <tynanjk@gmail.com>
2016-03-31 13:20:30 -0500

    First commit, basic HTML bage and two javascript folders added

---
+++ demo.js
@@ -0,0 +1,1 @@
+console.log('Oh look I\'m a test string!')
---
+++ demo2.js
@@ -0,0 +1,5 @@
+
+var name_of_function = function(){document.getElementById("javatest").style.color = "#ff0000"};
+
+
+console.log('It works!');
